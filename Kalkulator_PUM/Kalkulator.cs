b1f4a46720f0a7dbf8b4809bc890579e879f270a
Johnny <Johnny@DESKTOP-1G1RC10>
2017-04-26 21:01:22 +0200

    Add project files.

---
+++ Kalkulator_PUM/Kalkulator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator_PUM
+{
+    class Kalkulator
+    {
+        private double Wynik;
+
+        public void Dodaj(double x)
+        {
+            Wynik += x;
+        }
+
+        public void Odejmuj(double x)
+        {
+            Wynik -= x;
+        }
+
+        public void Mnozenie(double x)
+        {
+            Wynik *= x;
+        }
+
+        public void Dzielenie(double x)
+        {
+            Wynik /= x;
+        }
+    }
+}
